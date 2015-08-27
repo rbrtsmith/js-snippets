0ba9117d8a4433063445d4f660b1a78448457afc
Robert Smith <rbrtsmith84@googlemail.com>
2015-08-27 21:50:27 +0200

    Update getTotal.js

--- getTotal.js
+++ getTotal.js
@@ -1,5 +1,12 @@
+// ES5
 var getTotal = function(arr) {
   return arr.reduce(function(pv,cv){
     return pv + cv;
   });
 };
+
+//ES6
+const getTotal = (arr) =>
+  arr.reduce((pv, cv) =>
+    pv + cv
+  );
