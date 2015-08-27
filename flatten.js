aa0096826f861897a52442c9d20a1e8d21b79880
Robert Smith <rbrtsmith84@googlemail.com>
2015-08-27 21:46:46 +0200

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -1,5 +1,12 @@
+// ES5
 var flatten = function(arr) {
   return arr.reduce(function(prevVal, currentVal){
     return prevVal.concat(currentVal);
   });
 };
+
+
+// ES6
+const flatten = (arr) =>
+  arr.reduce((prevVal, currVal) =>
+    prevVal.concat(currVal));
