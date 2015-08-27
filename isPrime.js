248f432ce9bf3a23db62fdc7f211a6ed2c52d42a
Robert Smith <rbrtsmith84@googlemail.com>
2015-08-27 22:13:39 +0200

    Update isPrime.js

--- isPrime.js
+++ isPrime.js
@@ -1,11 +1,28 @@
-var isPrime = function(num) {
+// ES5
+var isPrime = function isPrime(num) {
+  var i = 2;
   if (num < 2) {
     return false;
   }
-  for (var i = 2; i<num; i++) {
+  for (i; i<num; i+=1) {
     if (num % i === 0) {
       return false;
     }
   }
   return true;
 };
+
+// ES6
+const isPrime1 = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i =2; i<num; i+=1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
