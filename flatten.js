ab5ca5fb263650da7d30f4e5de368eefb4ff0ef7
Robert Smith <rbrtsmith84@googlemail.com>
2015-09-05 09:17:13 +0200

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -1,12 +1,41 @@
 // ES5
-var flatten = function flatten(arr) {
-  return arr.reduce(function(pv, cv){
-    return pv.concat(cv);
+var containsArr = function(arr) {
+  return arr.some(function(val) {
+    if (typeof val === 'object') {
+      return true;
+    }
   });
 };
 
+var flatten = function(arr){
+  var result = [];
+  result = arr.reduce(function(pv, cv){
+    return pv.concat(cv);
+  },[]);
+  if (containsArr(result)) {
+    return flatten(result);
+  }
+  return result;
+};
 
-// ES6
-const flatten = (arr) =>
-  arr.reduce((pv, cv) =>
-    pv.concat(cv));
+
+
+//ES6
+const containsArr = arr => {
+  return arr.some(val => {
+    if (typeof val === 'object') {
+      return true;
+    }
+  });
+};
+
+const flatten = arr => {
+  let result = [];
+  result = arr.reduce((pv, cv) => {
+    return pv.concat(cv);
+  },[]);
+  if (containsArr(result)) {
+    return flatten(result);
+  }
+  return result;
+};
